321b19ed194322d2d9c0a4def746972fdfbf4390
Gimlettt <zy353@cam.ac.uk>
2022-10-12 10:20:20 +0100

    servo code

--- magnetsensing/magnetsensing.ino
+++ magnetsensing/magnetsensing.ino
@@ -2,11 +2,11 @@
 int hallreading;
 void setup(){
   Serial.begin(9600);
-  pinMode(4,OUTPUT);
+  pinMode(4,OUTPUT);//control LED lignt turn on and off
 }
 void loop(){
   hallreading=analogRead(hallsensorPin);
-  delay(500);
+  delay(500);//to be discussed the interval between 2 detecting
   if(hallreading<510){//can adjust accuracy
     Serial.println("found it");
     digitalWrite(4,HIGH);
