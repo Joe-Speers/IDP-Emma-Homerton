ecf631202b7b44cbd3381f68326024ca54d5fadd
orlahill <oh292@cam.ac.uk>
2022-10-16 10:33:12 +0100

    filtering distance sensor data

--- Ultra_sound/ultra_sound.ino
+++ Ultra_sound/ultra_sound.ino
@@ -1,3 +1,4 @@
+#include <Filters.h> //to apply exponential recursive filter
 
 // defines pins numbers
 const int trigPin = 9;
@@ -5,6 +6,9 @@ const int echoPin = 10;
 // defines variables
 long duration;
 int distance;
+//Filter for float numbers, (a,b) a is weighting of filter (to be calibrated), b is initial value of filter
+ExponentialFilter<float> FilteredDistance(90, 0);
+
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
@@ -22,7 +26,13 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
   distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
+  // Prints the distance on the Serial Monitor in cm
   Serial.print("Distance: ");
   Serial.println(distance);
+  //applying filter
+  FilteredDistance.Filter(distance);
+  float SmoothDistance = FilteredDistance.Current();
+  // print SmoothDistance in Serial monitor
+  Serial.print("Smoothed Distance: ")
+  Serial.println(SmoothDistance)
 }
