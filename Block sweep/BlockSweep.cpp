983d2ad4289d32fed0cb83d26962de0ab9ec1df2
kn393 <kn393@cam.ac.uk>
2022-10-18 10:46:24 +0100

    Module templates

---
+++ Block sweep/BlockSweep.cpp
@@ -0,0 +1,20 @@
+#include "include/BlockSweep.h"
+#include "IDP-Emma-Homerton/src/include/Motorcontrol.h"
+#include "IDP-Emma-Homerton/src/include/DistanceSense.h"
+
+void BlockSweep::BlockSwe(){
+    
+
+}
+        
+        
+int BlockSweep::AngleFind(){
+
+
+}
+
+
+void BlockSweep::DriveToBlock(){
+
+
+}
---
+++ Block sweep/include/BlockSweep.h
@@ -0,0 +1,16 @@
+#include "IDP-Emma-Homerton/src/include/util.h"
+
+class BlockSweep
+{
+    public:
+        int angle = 0;
+        int distance = 0;
+        
+        //
+        void BlockSwe();
+        int AngleFind();
+        void DriveToBlock();
+
+
+
+};
